2dc968b462febadfe58b6b251a547e122b88405e
BRYAN ONYANCHA <129788895+onyanchabryan1@users.noreply.github.com>
2023-04-05 11:44:38 +0300

    Update einstein.py

--- einstein.py
+++ einstein.py
@@ -1,6 +1,6 @@
 
 #CALCULATING THE ENERGY IN JUOLES USING THE EINSTEINS FORMULA
-mass=int(input("Enter your mass here(in kilograms)"))#prompt user to enter the mass in kilograms
-c=300000000#approximate speed of light in meters per second
-E=mass* (c)**2#ensteins formula to get energy E-mc2
-print("The enrgy in juoles is: ", E)#printing the result
+mass=int(input("Enter your mass here(in kilograms)"))  #prompt user to enter the mass in kilograms
+c=300000000  #approximate speed of light in meters per second
+E=mass* (c)**2  #ensteins formula to get energy E=mc2
+print("The enrgy in juoles is: ", E)   #printing the result
