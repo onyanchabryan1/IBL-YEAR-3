480158a8a21d3f795c6e53e290a2c4bfc829303e
BRYAN ONYANCHA <129788895+onyanchabryan1@users.noreply.github.com>
2023-04-05 21:05:57 +0300

    conditions in python:if,else,elif

---
+++ conditionals.py
@@ -0,0 +1,50 @@
+""""
+x=int(input("enter value of x"))
+y=int(input("enter value of y"))
+
+if x<y:
+    print("x is less than y")
+
+elif x>y:
+    print("x is greater than y")
+
+else :
+    print("x is equal to y")
+"""
+
+#logical operations
+"""
+x=int(input("enter value of x"))
+y=int(input("enter value of y"))
+
+if x<y or x>y:
+    print("x is not equal to y")
+
+else :
+    print("x is equal to y")
+
+if x==y:
+    print("x is equal to y")
+else :
+    print("x is not equal to y")
+    """
+  
+
+  #grading code
+
+score=int(input("score"))
+
+if score>=90:
+    print("grade A")
+
+elif score>=80:
+    print("grade B")
+
+elif score>=70:
+    print("grade c")
+
+elif score>=60:
+    print("grade D")
+
+else:
+    print("grade F")
