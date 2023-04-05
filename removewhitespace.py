ad6fb1fcc097c983bbf02008eedb8b301113d0cc
BRYAN ONYANCHA <129788895+onyanchabryan1@users.noreply.github.com>
2023-04-05 21:49:59 +0300

    this code removes whitespace using strip() function

---
+++ removewhitespace.py
@@ -0,0 +1,3 @@
+name=input("enter your name ")    #prompts user to enter the name
+name=name.strip()    #using strip() function to do away with whitespace
+print(name)    #print the name with no whitespace
