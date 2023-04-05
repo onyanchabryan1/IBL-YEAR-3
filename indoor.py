4957b0eff76091af09160f4b1efba656f39f4d51
BRYAN ONYANCHA <129788895+onyanchabryan1@users.noreply.github.com>
2023-04-05 05:42:03 +0300

    Add files via upload
    this code tekes input of a user in capital and outputs the same input in small letters

---
+++ indoor.py
@@ -0,0 +1,3 @@
+user_input=str(input("type your name in upper case"))#this pprompts the user to enter the name in capital letters
+lower_case_input=user_input.lower()#create another variable called lower_case_input which converts the first variable to small caps
+print(lower_case_input)#then print the second variable which is now in small caps
