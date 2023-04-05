c2e0f3d7e1c3fad835b87ad835eb995a8875caa2
BRYAN ONYANCHA <129788895+onyanchabryan1@users.noreply.github.com>
2023-04-05 11:48:59 +0300

    variable outside of a function used inside of a function

--- globalvariables.py
+++ globalvariables.py
@@ -2,4 +2,4 @@ name="bryan" #declaring a variable name with value bryan
 def myfunc():   #declaring the function
     print("His name is "+name)  #performing concatenation to combine the value with the function description
 
-myfunc()   #calling the function
+myfunc()    #calling the function
