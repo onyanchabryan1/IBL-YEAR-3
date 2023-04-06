b52da742c4cd198cb8a9f5f154759da7b63052bb
BRYAN ONYANCHA <129788895+onyanchabryan1@users.noreply.github.com>
2023-04-06 05:26:53 +0300

    promptsuser to anser great question of life

---
+++ deep.py
@@ -0,0 +1,6 @@
+answer=input("What is the answer to the great question of Life, the Universe and Everything? ")
+
+if answer.lower()in ["42","forty two","forty-two"]:
+    print("Yes")
+else:
+    print("No")
